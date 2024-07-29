6d337d26b33cb1864747fe78e59f9ed49cd726e8
Beatriz Alves <89867185+beatrizalves20@users.noreply.github.com>
2024-07-29 14:08:39 -0300

    Add files via upload

---
+++ Produto.py
@@ -0,0 +1,7 @@
+class Produto():
+    def __init__(self, nome: str, categoria: int, setor: int) -> None:
+        self.nome = nome
+        self.categoria = categoria
+        self.setor = setor
+        
+        
---
+++ Usuario.py
@@ -0,0 +1,6 @@
+class Usuario():
+    def __init__(self, id, nome, nascimento) -> None:
+        self.id = id
+        self.nome = nome
+        self.nascimento = nascimento
+    
